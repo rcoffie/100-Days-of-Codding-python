9272ad066d757f8d5b803ac0320f079f4fb65969
Robert Coffie <rcoffie22@yahoo.com>
2022-01-25 10:29:31 +0000

    day1

---
+++ main.py
@@ -0,0 +1,26 @@
+#String manupulation and code intelligence  
+
+#printing multiple lines with a single code 
+
+# print("hello world!\nHello world!\nHello world")
+
+#concatunating strings 
+
+# print("hello" + " " + "Robert")  
+# print("Day 1 - String Manipluation")
+# print("String Concatenation is donw with '+' sign.")
+# print("eg. print('hello' + 'world' )")
+
+#input challange
+# name = input ("what is your name ")
+# length = len(name) 
+# print(length)
+
+
+
+#Project 
+print("welcome to bhe band generator \n")
+city = input("which city did you grow up in \n")
+pet  = input("what is the name of your pet \n")
+
+print ("your band name is " + city + "" + pet)
